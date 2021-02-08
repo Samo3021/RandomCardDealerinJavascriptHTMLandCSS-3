d743406b7f4511e9273e1628285a36453b73f616
Samo3021 <samuelcampos15009@gmail.com>
2021-02-08 06:41:46 +0000

    falta los colores

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let color = ["red", "black"];
+  let pinta = ["\u2660", "\u2663", "\u2665", "\u25ca"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  var rdm_pinta = Math.floor(Math.random() * pinta.length);
+  var numero = Math.floor(Math.random() * numeros.length);
+  document.querySelector("#number").innerHTML =
+    "<p id=" + "centro" + ">" + numeros[numero] + "</p>";
+  document.querySelector("#superior").innerHTML =
+    "<p style=color: " + " id=" + "arriba" + ">" + pinta[rdm_pinta] + "</p>";
+  document.querySelector("#inferior").innerHTML =
+    "<p style=color: " + " id=" + "abajo" + ">" + pinta[rdm_pinta] + "</p>";
 };
